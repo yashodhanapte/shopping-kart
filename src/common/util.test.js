40c54d5b4e306ba0fd930a1b4f480f3fa242f43f
sumit lokhande <sumitlokhande858@gmail.com>
2021-07-22 23:16:49 +0530

    added linting validation to util.test.js

--- src/common/util.test.js
+++ src/common/util.test.js
@@ -9,15 +9,15 @@ describe('Test checkIfArrayLengthMoreThanLimit', () => {
     const moreThanlimit = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     const lessThanlimit = [1, 2, 3, 1, 4];
     const emptyArr = [];
-    test('test if function checkIfArrayLengthMoreThanLimit returns proper value if length >= limit ', () => {
+    test('1 test if function checkIfArrayLengthMoreThanLimit returns proper value if length >= limit', () => {
         const result = checkIfArrayLengthMoreThanLimit(moreThanlimit, limit);
         expect(result).toBeTruthy();
     })
-    test('test if function checkIfArrayLengthMoreThanLimit returns false value if length < limit ', () => {
+    test('2 test if function checkIfArrayLengthMoreThanLimit returns false value if length < limit', () => {
         const result = checkIfArrayLengthMoreThanLimit(lessThanlimit, limit);
         expect(result).toBeFalsy();
     })
-    test('test if function checkIfArrayLengthMoreThanLimit returns false value if length < limit ', () => {
+    test('3 test if function checkIfArrayLengthMoreThanLimit returns false value if length < limit', () => {
         const result = checkIfArrayLengthMoreThanLimit(emptyArr, limit);
         expect(result).toBeFalsy();
     })
